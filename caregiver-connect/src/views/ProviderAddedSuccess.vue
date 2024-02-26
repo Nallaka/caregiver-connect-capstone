74543c4664ede10b3b90639f7a20ce494519c76b
BrentmcUA <159183637+BrentmcUA@users.noreply.github.com>
2024-02-26 09:43:03 -0600

    Updated addProvider.vue
    Updated addProvider.vue so that a POST request is sent to the database when the add Provider button is pressed. Additionally, added a new dependency and it's a module called axios.

---
+++ caregiver-connect/src/views/ProviderAddedSuccess.vue
@@ -0,0 +1,20 @@
+<template>
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Provider Added Successfully</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <div>
+          <p>Provider has been successfully added!</p>
+          <!-- Display any relevant information about the newly added provider here -->
+        </div>
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script setup lang="ts">
+  // Other imports and variables
+  </script>
+  
